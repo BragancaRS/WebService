7f74b67a1accc7d1d3aed94a78b6d673f0fceb7d
BragancaRS <braganca.rs@gmail.com>
2017-04-26 10:35:38 -0300

    WebService

--- app/src/main/java/br/untins/webservice/MainActivity.java
+++ app/src/main/java/br/untins/webservice/MainActivity.java
@@ -1,8 +1,15 @@
 package br.untins.webservice;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -15,28 +22,37 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DownloadJsonInternet jsonInternet = new DownloadJsonInternet();
+        listView = (ListView) findViewById(R.id.listView);
+        DownloadJsonInternet jsonInternet = new DownloadJsonInternet(this);
         jsonInternet.execute("http://www.livroiphone.com.br/carros/carros_esportivos.json");
     }
 }
 
 
-class DownloadJsonInternet extends AsyncTask<String, Void, ModeloCarro>{
+class DownloadJsonInternet extends AsyncTask<String, Void, List<Carro>> {
+
+    MainActivity copy = null;
+
+    DownloadJsonInternet(MainActivity context) {
+        copy = context;
+    }
 
     //Codigo executado em uma nova thread(paralelo)
     @Override
-    protected ModeloCarro doInBackground(String... strings) {
+    protected List<Carro> doInBackground(String... strings) {
         OkHttpClient vrClienthttp = new OkHttpClient();
         Gson vrGson = new Gson();
 
         //Monta a requisição
         Request request = new Request.Builder().url(strings[0]).build();
 
-        try{
+        try {
             //Baixa o conteudo do webService
             Response response = vrClienthttp.newCall(request).execute();
 
@@ -45,7 +61,9 @@ class DownloadJsonInternet extends AsyncTask<String, Void, ModeloCarro>{
 
             //Converte a String JSON em Objetos do tipo carro
             ModeloCarro carros = vrGson.fromJson(sJson, ModeloCarro.class);
-        }catch (Exception e){
+
+            return carros.getCarros().getCarro();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -54,15 +72,43 @@ class DownloadJsonInternet extends AsyncTask<String, Void, ModeloCarro>{
 
     //Thread finalizaa (código executado na Thread principal) (Iterface)
 
-    protected void onPostExecute(ArrayList<ModeloCarro> carros) {
+    protected void onPostExecute(List<Carro> carros) {
 
+        Adapter adapter = new Adapter(copy, carros);
+        copy.listView.setAdapter(adapter);
     }
 
     //Prepara para excutar a Thread
     @Override
     protected void onPreExecute() {
 
+    }
+}
 
 
+class Adapter extends ArrayAdapter<Carro> {
+    MainActivity copy = null;
+
+    public Adapter(MainActivity context, List<Carro> vetCarros) {
+        super(context, 0, vetCarros);
+    }
+
+
+    public View getView(int position, View recycledCell, ViewGroup father) {
+
+        //Criar uma celula para cada elemento do vetor de carros
+        //Preencher essa celula com os dados do vetor de carros na Posicao position
+        Carro c = this.getItem(position);
+
+        if (recycledCell == null) {
+            recycledCell = LayoutInflater.from(getContext()).inflate(R.layout.celula, father, false);
+        }
+
+        //Pegar os dados do objeto e setar os elementos visuais da celula
+        TextView carModel = (TextView) recycledCell.findViewById(R.id.carModel);
+        carModel.setText(c.getNome());
+        //Setar imagem
+
+        return recycledCell;
     }
 }
